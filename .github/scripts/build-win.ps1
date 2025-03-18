063d486d9293ff650bece0766bda601971992105
Jan Bliznicenko <jan.bliznicenko@fit.cvut.cz>
2025-03-18 17:53:13 +0100

    added more Win script debug info

--- .github/scripts/build-win.ps1
+++ .github/scripts/build-win.ps1
@@ -10,6 +10,8 @@ $package_dir="$PROJECT_NAME-$PLATFORM"
 $vm_file_content=Get-Content $ci_build_dir/vm | Out-String
 $vm_dir=(($vm_file_content -replace '/[^/]*$','/pharo-vm') -replace '/c/','C:\') -replace '/','\'
 
+echo "Preparing files and directories..."
+
 mkdir $package_dir
 mkdir $package_dir/image
 mkdir $package_dir/Pharo
@@ -21,6 +23,8 @@ cp $ci_build_dir/*.sources $package_dir/image
 cp $vm_dir/*.dll $package_dir/Pharo
 cp $vm_dir/Pharo.exe $package_dir/Pharo
 cp $vm_dir/PharoConsole.exe $package_dir/Pharo
+
+echo "Building README..."
 
 "@echo off
 start `"`" `"%~dp0Pharo\Pharo.exe`" `"%~dp0image\$PROJECT_NAME.image`"" | set-content "$package_dir/$PROJECT_NAME.bat"
@@ -39,6 +43,10 @@ Opening may take several seconds. If OpenPonk becomes unresponsive almost immedi
 
 Get-Content -Path "ci-scripts\.github\scripts\readmecommon.txt" | Add-Content -Path "$package_dir/README.txt"
 
+echo "Saving OPVersion..."
+
 & $vm_dir/PharoConsole.exe -headless $package_dir/image/$PROJECT_NAME.image eval --save "OPVersion currentWithRunId: $RUN_ID projectName: '$REPOSITORY_NAME'"
+
+echo "Packaging zip archive..."
 
 Compress-Archive -Path $package_dir -DestinationPath "$PROJECT_NAME-$PLATFORM-$VERSION.zip"
